7cec9d0817f5e391ead13ab11deef2c5209b2ce7
David-Allen <davida.asencio@gmail.com>
2022-05-13 11:05:29 -0400

    application controller done

--- reddit_clone/reddit/app/controllers/application_controller.rb
+++ reddit_clone/reddit/app/controllers/application_controller.rb
@@ -15,12 +15,15 @@ class ApplicationController < ActionController::Base
 
   def logout!
     session[:session_token].reset_session_token! if logged_in?
+    session[:session_token] = nil
+    @curent_user = nil
   end
-
 
   def require_logged_in 
-    
+    redirect_to new_session_url unless logged_in?
   end
 
-
+  def require_logged_out
+    redirect_to users_url if logged_in?
+  end
 end
