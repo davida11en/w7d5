9a573158179d325661a6927755f4121bf4eab9d8
Duncan Murchison <duncancmurchison@gmail.com>
2022-05-13 10:58:24 -0400

    worked on users controller

--- reddit_clone/reddit/app/controllers/application_controller.rb
+++ reddit_clone/reddit/app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session: session[:session_token])
+  end
+
+  def logged_in?
+    !!curent_user
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    session[:session_token].reset_session_token! if logged_in?
+  end
+
+
+  def require_logged_in 
+    
+  end
+
+
 end
--- reddit_clone/reddit/app/controllers/users_controller.rb
+++ reddit_clone/reddit/app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, only: [:edit, :update, :show, :destory]
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to users_url
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+  
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:id, :password_digest, :session_token, :username)
+  end
+
+
 end
